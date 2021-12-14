fd322e4129817c4392daff34a9d96d2d1b577e36
David Soto <ds051128@gmail.com>
2021-12-13 19:44:20 -0600

    completed magic 8 ball

--- magic8.cpp
+++ magic8.cpp
@@ -7,5 +7,25 @@ int main() {
     srand(time(NULL));
     int answer = std::rand() % 10;
 
-    std::cout << answer;
+    if(answer == 0) {
+        std::cout << "It is certain.\n";
+    } else if (answer == 1) {
+        std::cout << "It is decidedly so.\n";
+    } else if (answer == 2) {
+        std::cout << "Without a doubt.\n";
+    } else if (answer == 3) {
+        std::cout << "Yes - definitely.\n";
+    } else if (answer == 4) {
+        std::cout << "You may rely on it.\n";
+    } else if (answer == 5){
+        std::cout << "As I see it, yes.\n";
+    } else if (answer == 6){
+        std::cout << "Most likely.\n";
+    } else if (answer == 7){
+        std::cout << "Reply hazy, try again.\n";
+    } else if (answer == 8){
+        std::cout << "Don't count on it.\n";
+    } else {
+        std::cout << "Very doubtful.\n";
+    }
 }
