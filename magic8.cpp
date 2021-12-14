ea1b3fe76ec1ff7753ec488e977e8a98d034527f
David Soto <ds051128@gmail.com>
2021-12-13 19:31:05 -0600

    generating random number from 0-9

--- magic8.cpp
+++ magic8.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 
 int main() {
-    std::cout << "MAGIC 8-BALL:\n";
-
-    int answer = std::rand();
+    std::cout << "MAGIC 8-BALL:\n\n";
+    srand(time(NULL));
+    int answer = std::rand() % 10;
 
     std::cout << answer;
 }
